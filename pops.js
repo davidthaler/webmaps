7cae7801f2736af68a009e7e0d9366dfe5535c8a
David Thaler <davidthaler@gmail.com>
2019-06-04 21:45:07 -0700

    Wrote process_pops.py to make hand-rolled geoJSON feature collection (Point), from privately owned public space data. Data file pops.js is just the output of that, running on POPS.csv from City of Seattle.

---
+++ pops.js
@@ -0,0 +1,621 @@
+var pops = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Children's Hospital ER Addition",
+                "year": 2010
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.2844,
+                    47.6614704
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Epiphany School",
+                "year": 2008
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.28724,
+                    47.6182563
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Ruby (Amazon)",
+                "year": 2012
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33676,
+                    47.6214019
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Rufus (Amazon)",
+                "year": 2008
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33645,
+                    47.6239012
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Skyline First Hill",
+                "year": 2006
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.32639,
+                    47.6060183
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Doppler Tower (Amazon)",
+                "year": 2012
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33851,
+                    47.6151253
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Enso Condominiums",
+                "year": 2007
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33922,
+                    47.6181048
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "1111 3rd Ave",
+                "year": 1977
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.3354,
+                    47.6066096
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Safeco Plaza",
+                "year": 1969
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33353,
+                    47.6063709
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "1000 2nd Avenue",
+                "year": 1987
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33489,
+                    47.6054034
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Wells Fargo Center",
+                "year": 1983
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33466,
+                    47.6050898
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Fourth & Madison Building",
+                "year": 2002
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33308,
+                    47.6056501
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "The Post",
+                "year": 2011
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33609,
+                    47.6033063
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Columbia Center",
+                "year": 1981
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33078,
+                    47.6045379
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "800 Fifth Avenue",
+                "year": 1981
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33025,
+                    47.605773
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Seattle Municipal Tower",
+                "year": 1980
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.32996,
+                    47.6050437
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Passport Agency",
+                "year": 2003
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.32723,
+                    47.6018316
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Union Station Development",
+                "year": 2000
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.32809,
+                    47.5975244
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "2020 E Madison St",
+                "year": 2016
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.30504,
+                    47.6181037
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "The Whittaker",
+                "year": 2014
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.38191,
+                    47.5601124
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Nessie (Amazon)",
+                "year": 2012
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33902,
+                    47.6238654
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Alder Hall UW",
+                "year": 2009
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.31395,
+                    47.6556098
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Elm Hall UW",
+                "year": 2010
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.31493,
+                    47.6565191
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Admiral Safeway",
+                "year": 2010
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.38638,
+                    47.579426
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Expo Apts",
+                "year": 2010
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.35458,
+                    47.6234256
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Horizon House",
+                "year": 1954
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.32907,
+                    47.6108681
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Cielo Apartments",
+                "year": 2012
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.3297,
+                    47.6095649
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "One/Two Union Square",
+                "year": 1979
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33281,
+                    47.6100881
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "WA State Convention Center",
+                "year": 1990
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33219,
+                    47.6115035
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Westin Building",
+                "year": 1982
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33884,
+                    47.6143481
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Van Vorst Building (Amazon)",
+                "year": 2005
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33676,
+                    47.6226246
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Park Place",
+                "year": 1971
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33206,
+                    47.6090297
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Crowne Plaza Hotel",
+                "year": 1973
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33189,
+                    47.6080525
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "1600 Seventh",
+                "year": 1973
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33472,
+                    47.6135625
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "8th + Olive",
+                "year": 1981
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33476,
+                    47.6142306
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Plaza 600",
+                "year": 1969
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33719,
+                    47.614032
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "US Bank Centre",
+                "year": 1986
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33468,
+                    47.6105424
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Century Square",
+                "year": 1983
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.3381,
+                    47.6104009
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Benaroya Hall",
+                "year": 1995
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33698,
+                    47.6075664
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Seattle Art Museum",
+                "year": 1991
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33767,
+                    47.6072142
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Four Seasons Hotel",
+                "year": 2005
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33917,
+                    47.6073756
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "1201 3rd Ave",
+                "year": 1990
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33619,
+                    47.6071864
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Fourth & Blanchard Building",
+                "year": 1977
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.34185,
+                    47.6137881
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Sixth & Lenora Building",
+                "year": 1963
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.33935,
+                    47.6147682
+                ]
+            }
+        }
+    ]
+};
---
+++ process_pops.py
@@ -0,0 +1,61 @@
+'''
+Input the City of Seattle's csv data on publicly-owned open spaces and output as geojson.
+In the csv, the coordinates are not in a form to make valid JSON.
+
+author: David Thaler
+date: June 3, 2019
+'''
+import argparse
+import pandas as pd 
+import json
+
+# default path to write to
+POPS_PATH = '../../data/POPS.csv'
+
+def load_pops(infile):
+    'Load the part of the csv file that we use'
+    pops = pd.read_csv(infile)
+    x = (pops[['Common Name', 'Location', 'Address', 'Built']]
+        .rename({'Common Name': 'Name', 'Built': 'Year'}, axis=1))
+    #x['Coordinates'] = x.Location.map(lambda s: s.replace('(', '[').replace(')', ']'))
+    x['Coordinates'] = pops.Location.map(lambda s: [float(n) for n in s.strip('()').split(',')][::-1])
+    return x.drop('Location', axis=1)
+
+def make_feature(r):
+    'Makes one geoJSON feature for a feature collection from one row of POPS data frame.'
+    properties = {'name': r.Name, 'year': r.Year}
+    geometry = {'type': 'Point', 'coordinates': r.Coordinates}
+    return {'type': 'Feature', 'properties': properties, 'geometry': geometry}
+
+def make_collection(x):
+    'Makes a valid geoJSON feature collection (Point) from POPS data frame.'
+    features = x.apply(make_feature, axis=1)
+    return {'type': 'FeatureCollection', 'features': list(features)}
+
+def output_collection(d, out, indent=1):
+    '''
+    Output the dict from make_collection to a file as valid javascript.
+
+    Args:
+        d: the dict with valid geojson
+        out: output file or path
+        indent: number of spaces to indent at each level for pretty-printing.
+            Use None for packed (smallest).
+    '''
+    with open(out, 'w') as f:
+        gj = json.dumps(d, indent=indent)
+        f.write('var pops = ')
+        f.write(gj)
+        f.write(';')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Read in the POPS data (csv) from the City, output geojson')
+    parser.add_argument('--infile', help='Input csv file', default=POPS_PATH)
+    parser.add_argument('--outfile', help='output (javascript) file containing a geojson variable')
+    parser.add_argument('--indent', type=int, help='spaces to indent in pretty printing', default=1)
+    args = parser.parse_args()
+    x = load_pops(args.infile)
+    gj = make_collection(x)
+    output_collection(gj, args.outfile, args.indent)
